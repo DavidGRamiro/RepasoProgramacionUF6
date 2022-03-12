23dc7941de353043672667c94523fda33e381ddf
David González <davidgramiro@gmail.com>
2022-03-12 21:21:33 +0100

    Aplico metodos get y set

--- src/centroEducativo/Persona.java
+++ src/centroEducativo/Persona.java
@@ -7,12 +7,37 @@ public abstract class Persona {
 	private String dirección;
 	private  int telf;
 	
-
+	public Persona(String nombre,String nif,String dirección,int telf) {
+		this.nombre=nombre;
+		this.nif=nif;
+		this.dirección=dirección;
+		this.telf=telf;
+	}
 	
-	
-	
-	
-	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	public String getNif() {
+		return nif;
+	}
+	public void setNig(String nif) {
+		this.nif=nif;
+	}
+	public String dirección() {
+		return dirección;
+	}
+	public void setDirección(String dirección) {
+		this.dirección=dirección;
+	}
+	public int getTelf() {
+		return telf;	
+	}
+	public void setTelf(int telf) {
+		this.telf=telf;
+	}
 	
 	
 }
