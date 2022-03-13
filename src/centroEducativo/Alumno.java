a163e09d407a25f6e28edbc4dfdfe807208bf23f
David González <davidgramiro@gmail.com>
2022-03-13 09:57:43 +0100

    Añado atributos de clase y métodos get/set

--- src/centroEducativo/Alumno.java
+++ src/centroEducativo/Alumno.java
@@ -2,4 +2,18 @@ package centroEducativo;
 
 public class Alumno {
 
+	private String curso;
+	
+	public Alumno(String curso) {	
+		this.curso = curso;
+	}
+	public Alumno() {
+	}
+	public String gertCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso=curso;
+	}
+	
 }
