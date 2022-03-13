65116be0db1b649a2ea9767f24c4031a522ed690
David González <davidgramiro@gmail.com>
2022-03-13 09:53:41 +0100

    Arreglo fallos en metodos

--- src/centroEducativo/Administrativo.java
+++ src/centroEducativo/Administrativo.java
@@ -7,12 +7,30 @@ public class Administrativo extends Persona {
 	public Administrativo(String tareas) {
 		this.tareas=tareas;
 	}
+	public Administrativo() {
+		
+	}
 	public String getTareas() {
 		return tareas;
 	}
 	public void setTareas(String tareas) {
 		this.tareas=tareas;
 	}
+	
+	public String gestionarMatricula() {
+		return "La administrativa" + this.getNombre() + "va a gestionar las matriculas";
+	}
+	public String trabajar() {
+		return "La administrativa " + this.getNombre() + "va a realizar estas tareas: " + this.getTareas();
+	}
+	public String llamar() {
+		return this.getNombre() + "llamando a " + this.getNombre();
+	}
+	@Override
+	public String toString() {
+		 return "Tareas: " + this.getTareas();
+	}
+	
 	
 }
 
