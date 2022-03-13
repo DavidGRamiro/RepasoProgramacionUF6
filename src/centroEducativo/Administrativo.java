f2b84d26a1a45000329a833bd9849fa5417fb19e
David González <davidgramiro@gmail.com>
2022-03-13 09:39:57 +0100

    Creo atributos de clase administrativo y constructor

--- src/centroEducativo/Administrativo.java
+++ src/centroEducativo/Administrativo.java
@@ -1,5 +1,18 @@
 package centroEducativo;
 
-public class Administrativo {
+public class Administrativo extends Persona {
 
+	private String tareas;
+	
+	public Administrativo(String tareas) {
+		this.tareas=tareas;
+	}
+	public String getTareas() {
+		return tareas;
+	}
+	public void setTareas(String tareas) {
+		this.tareas=tareas;
+	}
+	
 }
+
